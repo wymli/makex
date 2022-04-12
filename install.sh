5667787463bcdeb13980d13e3e6fe7e9b3e6d7b2
liwm29 <liwm29@mail2.sysu.edu.cn>
2022-04-12 21:11:24 +0800

    feat: update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,26 @@
+# run `curl -fsSL https://raw.githubusercontent.com/wymli/makex/master/install.sh | INSTALL_TYPE=release sh -` to install from remote.
 
-# build from scrach
-git clone https://github.com/wymli/makex.git
-cd makex
-go build
-sudo cp makex /usr/local/bin
+
+case $INSTALL_TYPE in
+  build)
+    # build from scrach
+    git clone https://github.com/wymli/makex.git
+    pushd makex
+    go build
+    sudo mv makex /usr/local/bin
+    popd
+    rm -rf makex
+    ;;
+  release)
+    # download release
+    url=https://github.com/wymli/makex/releases/latest/download/makex
+    curl -fsSL url > makex
+    chmod +x makex
+    sudo mv makex /usr/local/bin
+    ;;
+  *)
+    echo "not supported"
+    ;;
+esac
+
+echo "makex installed: $(which makex)"
